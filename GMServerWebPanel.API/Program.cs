db7ad1cb451a64e5d07ac094925f10fc29a197dd
seray-orig <serayplayplay@gmail.com>
2026-06-14 22:58:13 +0300

    фикс порта бэкэнда.

--- GMServerWebPanel.API/Program.cs
+++ GMServerWebPanel.API/Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5500")
+        policy.WithOrigins("http://localhost")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
